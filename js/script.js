4758847a4397e6872839eac9e27409370ad54db9
nablith <nablith@gmail.com>
2016-01-20 18:55:30 -0600

    made my rocket!

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
-		
 
+//
+var doCoolStuff = function () {
+	var currentClassName = document.getElementById('cool').className;
+// changing class name
+	if (currentClassName == 'cool') {
+		document.getElementById('cool').className = 'cool red';
+	} else {
+		document.getElementById('cool').className = 'cool';
+	}
 
+}
 
+var sayMyName = function (name) {
+	alert('My name is' +name);
+}
 
 
 var car = {
